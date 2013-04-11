8e32fc91811f5efc5b495730b59f509c1a784260
Jim Greer <jim@kongregate.com>
2013-04-11 13:43:12 -0700

    Inline standings

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,25 @@
+// One object per tournmanent, in chronological order
+var results = 
+    [
+        {"Gregarious Narain": 265, "Charlie Ansanelli": 265, "Brett Suwyn": 140, "Romy Mancini": 80, "Chris Burkhart": 30},
+        {"David Selle": 290, "Scott Waugaman": 190, "Sam Wyman": 130, "Justin Bailey": 70, "Jim Greer": 40 },
+        {"Jim Greer": 185, "Sam Wyman": 185, "Unni Narayanan": 90, "Aaron Wasserman": 50,  "Romy Mancini": 30 },
+    ]
+;
+
+var affilliations = {
+  "Aaron Wasserman": "Blackboard Mobile",
+  "Brett Suwyn": "Chute",
+  "Charlie Ansanelli": "Treasure Island Flea",
+  "Chris Burkhart": "Chute",
+  "David Selle": "Nukotoys",
+  "Gregarious Narain": "Chute",
+  "Jim Greer": "Kongregate",
+  "Justin Bailey": "Double Fine",
+  "Sam Wyman": "Blackboard Mobile",
+  "Scott Waugaman": "Capcom",
+  "Unni Narayanan": "Mind Pirate",
+};
 
 function write_results(results) {
   results = sort_results(results);
