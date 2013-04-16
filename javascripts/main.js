f408227d268f7fbb90d5eabcaea29c01afb40c67
Poker <poker@Jims-MacBook-Pro-2.local>
2013-04-15 23:07:02 -0700

    week 4 glory

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,11 +4,13 @@ var results =
         {"Gregarious Narain": 265, "Charlie Ansanelli": 265, "Brett Suwyn": 140, "Romy Mancini": 80, "Chris Burkhart": 30},
         {"David Selle": 290, "Scott Waugaman": 190, "Sam Wyman": 130, "Justin Bailey": 70, "Jim Greer": 40 },
         {"Jim Greer": 185, "Sam Wyman": 185, "Unni Narayanan": 90, "Aaron Wasserman": 50,  "Romy Mancini": 30 },
+        {"Jim Greer": 210, "Tim Conkling": 140, "Brett Suwyn": 90, "Justin Bailey":50, "Bill Sandberg":30}
     ]
 ;
 
 var affilliations = {
   "Aaron Wasserman": "Blackboard Mobile",
+  "Bill Sandberg": "Zedo",
   "Brett Suwyn": "Chute",
   "Charlie Ansanelli": "Treasure Island Flea",
   "Chris Burkhart": "Chute",
